923e24558967cb3cda30790c2984e5e188c3f624
JustKooba <109106079+JustKooba@users.noreply.github.com>
2022-10-15 19:44:23 +0200

    Add files via upload

---
+++ randomjoke/script.js
@@ -0,0 +1,19 @@
+// grabbing a button
+const button = document.querySelector(".container button");
+// grabing "p"
+const jokeDiv = document.querySelector(".container .joke p");
+
+document.addEventListener("DOMContentLoaded", getJoke);
+
+button.addEventListener("click", getJoke);
+
+async function getJoke() {
+  const jokeData = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const jokeObj = await jokeData.json();
+  jokeDiv.innerHTML = jokeObj.joke;
+  console.log(jokeData);
+}
